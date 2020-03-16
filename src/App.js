2b9954534036b46da5f692f0bd2cdd9b2be46518
kevinsbstn4 <kevincaesar22@gmail.com>
2020-03-16 20:15:44 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "../src/components/common/Navbar";
+import "./App.css";
+import { Switch } from "@material-ui/core";
+import { Route } from "react-router-dom";
+import HomePage from "./components/home/HomePage";
+import AboutPage from "./components/home/AboutPage";
+import CarsPage from "./components/cars/CarsPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/about" component={AboutPage} />
+      <Route path="/cars" component={CarsPage} />
     </div>
   );
 }
